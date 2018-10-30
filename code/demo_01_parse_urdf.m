48cce9a119b9a7e48b55cd23b0d5a460635d1b3d
sjchoi86 <sungjoon.choi@cpslab.snu.ac.kr>
2018-10-30 08:47:18 -0700

    fig_simple added

--- code/demo_01_parse_urdf.m
+++ code/demo_01_parse_urdf.m
@@ -3,7 +3,7 @@ ccc
 ccc
 
 % Load the model
-model_name = 'franka'; % franka / sawyer
+model_name = 'sawyer'; % franka / sawyer
 urdf_path = sprintf('model/%s/%s_urdf.xml',model_name,model_name);
 base_name = 'world_fixed'; VERBOSE = true;
 model = parse_urdf(model_name,urdf_path,base_name,VERBOSE);
@@ -18,9 +18,9 @@ names2control = model.joint_names_revolute;
 positions2control_rad = zeros(1,length(names2control));
 model = set_positions_urdf(model,names2control,positions2control_rad,VERBOSE);
 
-% Plot the kinematic chain
+% Plot the kinematic chain with mesh
 opt = struct('fig_idx',2,'subfig_idx',1,...
-    'view_info',[116,17],'fig_size',[0.4,0.2,0.55,0.7],...
+    'view_info',[120,24],'fig_size',[0.4,0.2,0.55,0.7],...
     'ball_r',0.02,'coord_r',0.05,...
     'title_str',model_name,'title_fs',25,...
     'axis_info',[-1.5,+1.5,-1.5,+1.5,0.0,1.6],...
@@ -29,11 +29,25 @@ opt = struct('fig_idx',2,'subfig_idx',1,...
 fig_model = plot_model_urdf(model,opt);
 drawnow;
 
+% Plot the simple kinematic chain
+opt = struct('fig_idx',3,'subfig_idx',1,...
+    'view_info',[120,24],'fig_size',[0.4,0.1,0.55,0.7],...
+    'ball_r',0.1,'coord_r',0.1,...
+    'title_str',model_name,'title_fs',25,...
+    'axis_info',[-1.5,+1.5,-1.5,+1.5,0.0,1.6],...
+    'SHOW_TEXT',1,'PLOT_LINKS',1,'PLOT_JOINTS',1,'PLOT_COORDINATES',1, ...
+    'PLOT_ROTATIONAL_AXES',1,'PLOT_MESH',0,'PLOT_CUBE',0);
+fig_simple = plot_model_urdf(model,opt);
+drawnow;
+
+
 % Save figure
 png_name = sprintf('fig/fig_node_%s.png',model_name);
 save_fig2png(fig_node,png_name,VERBOSE);
 png_name = sprintf('fig/fig_model_%s.png',model_name);
 save_fig2png(fig_model,png_name,VERBOSE);
+png_name = sprintf('fig/fig_simple_%s.png',model_name);
+save_fig2png(fig_simple,png_name,VERBOSE);
 
 %% Plot random positions
 max_tick = 10;
